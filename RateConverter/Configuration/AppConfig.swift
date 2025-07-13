26f8b9a7a1d923003c171b8f8404057b90b9d074
Chao Ming Huang <mingqed@gmail.com>
2025-07-13 15:25:59 +0800

    Add 'AppConfig'

---
+++ RateConverter/Configuration/AppConfig.swift
@@ -0,0 +1,13 @@
+//
+//  AppConfig.swift
+//  RateConverter
+//
+//  Created by 黃昭銘 on 2025/7/11.
+//
+
+import Foundation
+
+enum AppConfig {
+    static let baseURL = URL(string: "https://openexchangerates.org/api/")!
+    static let appID = "9655c63914c648e58c1ed5f8c97c61f6"
+}
