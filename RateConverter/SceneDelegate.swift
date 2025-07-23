5ec410686f6c4e463de0a0d61b783526fb36d9f2
Chao Ming Huang <mingqed@gmail.com>
2025-07-23 22:31:23 +0800

    Setup 'SceneDelegate'

--- RateConverter/SceneDelegate.swift
+++ RateConverter/SceneDelegate.swift
@@ -13,10 +13,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+       
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let httpClient = URLSessionHTTPClient(session: URLSession.shared)
+        let storeURL = FileManager.default.temporaryDirectory.appendingPathComponent(AppConfig.currenciesCacheFileName)
+        let cacheStore = ActorCodableCacheStoreWithExpiry(expiryTimeInterval: AppConfig.cacheExpiryInterval, storeURL: storeURL)
+        
+        let remoteRepoitory = RemoteCurrenciesRepository(client: httpClient)
+        let storeRepository = StoreCurrenciesRepository(store: cacheStore)
+        
+        let getCurrenciesUseCase = GetCurrenciesUseCase(remoteRepository: remoteRepoitory, storeRepository: storeRepository)
+        let convertCurrenciesUseCase = ConvertCurrenciesUseCase()
+        
+        let viewModel = ConverterViewModel(getCurrenciesUseCase: getCurrenciesUseCase, convertCurrenciesUseCase: convertCurrenciesUseCase)
+        
+        let rootViewController = ConverterViewController(viewModel: viewModel)
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = rootViewController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
