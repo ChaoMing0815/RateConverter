7a638c530ee38491a40e065189d7f86c7ad27758
Chao Ming Huang <mingqed@gmail.com>
2025-07-25 21:24:06 +0800

    Modify 'AppConfig'

--- RateConverter/Configuration/AppConfig.swift
+++ RateConverter/Configuration/AppConfig.swift
@@ -12,7 +12,7 @@ enum AppConfig {
     static let appID = "9655c63914c648e58c1ed5f8c97c61f6"
     
     static let currenciesCacheFileName = "currencies_info.json"
-    static let cacheExpiryInterval: TimeInterval = 60 * 30 // 30 mins
+    static let cacheExpiryInterval: TimeInterval = 60 * 60 // data will to be renew every hour
     
     static let defaultSelectedCurrency = "USD"
 }
